c845cccc71f900c2c478e1ad7dbd1f692e719a32
anshkapade <anushri3kapade@gmail.com>
2023-12-26 04:15:31 -0800

    done first variable assignment
    done first variable assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,43 @@
+var myFullName;
+console.log(myFullName);
+
+ myFullName = "Anushri Kapade" ;
+ console.log("My Full Name :",myFullName);
+
+ myFullName = "Anushri Anil Kapade";
+ console.log("Complete Name:",myFullName);
+
+ console.log("------------------------------");
+
+ var myLove = "i love only JS";
+ console.log("my love is:",myLove);
+
+ console.log("-------------------------------------");
+
+ var firstName = "Anushri";
+ var lastName = "Kapade";
+ var age = "24";
+ var address = "Sadashiv Peth , Pune";
+ var pinCode = "411057";
+ var gender= "Female";
+ var marritalStatus = "Unmarried";
+
+ console.log("my First Name :",firstName);
+ console.log("my Last Name :",lastName);
+ console.log("Age :",age);
+ console.log("Address :",address);
+ console.log("PinCode:",pinCode);
+ console.log("Gender :",gender);
+ console.log("Marrital Status:",marritalStatus);
+ 
+ 
+
+
+
+
+
+
+
+
+
+
