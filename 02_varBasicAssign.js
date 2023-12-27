a94a932fb0c6e603b8cdf0ac69a5ae5f4b394e2c
anshkapade <anushri3kapade@gmail.com>
2023-12-27 03:55:30 -0800

    done variable assignment

---
+++ 02_varBasicAssign.js
@@ -0,0 +1,30 @@
+console.log("===============================Number DataType=============================");
+var age = 40;
+console.log("Age :",age ,",","type :" ,typeof age);
+
+var mobNum = 9876564323;
+console.log("Mobile No. :",mobNum,",","type :", typeof mobNum);
+
+var studentTotal = 50;
+console.log("Total No. of Student :",studentTotal,",","type :", typeof studentTotal);
+
+console.log("===============================String DataType=============================");
+var name = "Anushri";
+console.log("Name :", name ,",","type :", typeof name );
+
+var schoolName = "Saraswati Vidyalaya";
+console.log("Nmae of School :",schoolName,",","type:" ,typeof schoolName);
+
+var district ="Sangli";
+console.log("District :", district,",","type:", typeof district);
+
+
+console.log("===============================Boolean DataType=============================");
+var isPass = true;
+console.log("Pass :",isPass,",","type:", typeof isPass);
+
+var married =false;
+console.log("Married :",married,",","type:", typeof married);
+
+var retired = true;
+console.log("Retired :",retired,",","type:", typeof retired);
